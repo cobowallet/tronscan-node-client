9b8cbf52d0ec7fbbc8bef6d9c0569a8dc93980c6
huazhouwang <huazhou19@gmail.com>
2018-11-22 14:27:08 +0800

    optimize test

--- test/transactionBuilder.js
+++ test/transactionBuilder.js
@@ -1,33 +1,77 @@
-const buildWithdrawBalance = require("../src/utils/transactionBuilder").buildWithdrawBalance;
-const buildUnfreezeBalance = require("../src/utils/transactionBuilder").buildUnfreezeBalance;
-const buildFreezeBalance = require("../src/utils/transactionBuilder").buildFreezeBalance;
-const byteArray2hexStr = require("../src/utils/bytes").byteArray2hexStr;
-const buildVote = require("../src/utils/transactionBuilder").buildVote;
-const { assert } = require('chai');
+const buildWithdrawBalance = require('../src/utils/transactionBuilder')
+  .buildWithdrawBalance;
+const buildUnfreezeBalance = require('../src/utils/transactionBuilder')
+  .buildUnfreezeBalance;
+const buildFreezeBalance = require('../src/utils/transactionBuilder')
+  .buildFreezeBalance;
+const byteArray2hexStr = require('../src/utils/bytes').byteArray2hexStr;
+const buildVote = require('../src/utils/transactionBuilder').buildVote;
+const assert = require('assert');
+
+const toRawHex = tx => byteArray2hexStr(tx.getRawData().serializeBinary());
 
 describe('transactionBuilder', () => {
-
   it('build voteContract', async () => {
-    let transaction = buildVote("TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx", {
-      "TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx": 100
+    const tx = buildVote('TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx', {
+      TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx: 100,
     });
-
-    console.log("hex", byteArray2hexStr(transaction.getRawData().serializeBinary()));
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A6A080412660A30747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E566F74655769746E657373436F6E747261637412320A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E0012190A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E001064',
+    );
   });
 
-  it('build freeze', async () => {
-    let transaction = buildFreezeBalance("TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx", 100000000, 3);
-    console.log("hex", byteArray2hexStr(transaction.getRawData().serializeBinary()));
+  it('build freeze (bandwidth default)', async () => {
+    const tx = buildFreezeBalance(
+      'TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx',
+      100000000,
+      3,
+    );
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A58080B12540A32747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E467265657A6542616C616E6365436F6E7472616374121E0A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E001080C2D72F1803',
+    );
+  });
+
+  it('build freeze (bandwidth explicit)', async () => {
+    const tx = buildFreezeBalance(
+      'TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx',
+      100000000,
+      3,
+      'BANDWIDTH',
+    );
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A58080B12540A32747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E467265657A6542616C616E6365436F6E7472616374121E0A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E001080C2D72F1803',
+    );
+  });
+
+  it('build freeze (energy explicit)', async () => {
+    const tx = buildFreezeBalance(
+      'TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx',
+      100000000,
+      3,
+      'ENERGY',
+    );
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A5A080B12560A32747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E467265657A6542616C616E6365436F6E747261637412200A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E001080C2D72F18035001',
+    );
   });
 
   it('build unfreeze', async () => {
-    let transaction = buildUnfreezeBalance("TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx");
-    console.log("hex", byteArray2hexStr(transaction.getRawData().serializeBinary()));
+    const tx = buildUnfreezeBalance('TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx');
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A53080C124F0A34747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E556E667265657A6542616C616E6365436F6E747261637412170A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E00',
+    );
   });
 
   it('build withdraw', async () => {
-    let transaction = buildWithdrawBalance("TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx");
-    console.log("hex", byteArray2hexStr(transaction.getRawData().serializeBinary()));
+    const tx = buildWithdrawBalance('TKcrAJN3tgLshGqp7aTDTJLabdWqZSKFMx');
+    assert.strict.equal(
+      toRawHex(tx),
+      '5A53080D124F0A34747970652E676F6F676C65617069732E636F6D2F70726F746F636F6C2E576974686472617742616C616E6365436F6E747261637412170A154169D78AAC0544126C5199CFF1E1BDC94B15FE9E00',
+    );
   });
-
 });
